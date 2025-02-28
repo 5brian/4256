0a7265ae0ef25421feb8de8b57ee4098284d4eee
brian tan <brian.tan88@gmail.com>
2025-02-28 15:51:39 -0500

    +++

--- labs/lab_6.py
+++ labs/lab_6.py
@@ -54,3 +54,62 @@ def cartesian_product(s1,s2):
             result.add((item1, item2))
 
     return result
+
+# section 2
+
+def union_2(*sets):
+    result = set()
+
+    for s in sets:
+        for item in s:
+            result.add(item)
+
+    return result
+
+def isdisjoint_2(*sets):
+    for i in range(len(sets)):
+        for j in range(i + 1, len(sets)):
+            for item in sets[i]:
+                if item in sets[j]:
+                    return False
+
+    return True
+
+# section 3
+
+def is_well_formed(s):
+    stack = []
+    brackets = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in '({[':
+            stack.append(char)
+        elif char in ')}]':
+            if not stack or stack.pop() != brackets[char]:
+                return False
+
+    return len(stack) == 0
+
+# section 4
+
+def rpn(s):
+    stack = []
+
+    for char in s:
+        if char.isdigit():
+            stack.append(int(char))
+        else:
+            if len(stack) < 2:
+                raise ValueError("Invalid RPN expression")
+
+            b = stack.pop()
+            a = stack.pop()
+
+            if char == '+':
+                stack.append(a + b)
+            elif char == '-':
+                stack.append(a - b)
+            elif char == '*':
+                stack.append(a * b)
+
+    return stack[0]
